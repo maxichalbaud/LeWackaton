8df4e4cc9b13b19d51226744d3075521bb167cc6
Santiago Llapur <santillapur@gmail.com>
2020-08-08 16:41:46 -0300

    refact[seeds]: Add users and variables names

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Company.create(name: "IBM")
-Company.create(name: "Globant")
-Company.create(name: "Auth0")
+#Users
+pedro = User.new(email: "pedro@mail.com", password: '123456789', first_name: 'Pedro', last_name: 'Fernandez', avatar: 'https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80')
+hernan = User.new(email: 'hernan@mail.com', password: '123456789', first_name: 'Hernán', last_name: 'Ayala', avatar: 'https://images.unsplash.com/photo-1564564244660-5d73c057f2d2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1355&q=80')
 
-Course.create(title: "Data Science", company: Company.find_by(name:"IBM"))
-Course.create(title: "Data Science", company: Company.find_by(name:"Globant"))
-Course.create(title: "Data Science", company: Company.find_by(name:"Auth0"))
+
+#Companies
+ibm = Company.create(name: "IBM")
+globant = Company.create(name: "Globant")
+auth0 = Company.create(name: "Auth0")
+
+#Courses
+Course.create(title: "Data Science", company: ibm)
+Course.create(title: "Data Science", company: globant)
+Course.create(title: "Data Science", company: auth0)
