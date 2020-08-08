495489a8e7ee0188789430f9f8b7ac7b307e0c2a
Maxi Chalbaud <maxichalbaud@gmail.com>
2020-08-08 16:01:55 -0300

    Added search logic at pages#home and courses#index controller, added root_path to navbar logo, added images to seeds

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,15 @@
 class CoursesController < ApplicationController
-  before_action :set_course
+  before_action :set_course, only: [:show]
+
+  def index
+    @courses = Course.order(created_at: :desc)
+    if params[:category].present?
+      sql_query = "category = :category"
+      @courses = Course.where(sql_query, category: Course.categories[params[:category]])
+    elsif params[:company].present?
+      @courses = Course.where(company: Company.find(params[:company]))
+    end
+  end
 
   def show
     @enrollement = UserCourse.new
