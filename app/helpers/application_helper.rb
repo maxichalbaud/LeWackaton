4edeec60a706cff32be60b79d0931a96c740d879
Maxi Chalbaud <maxichalbaud@gmail.com>
2020-08-09 17:04:10 -0300

    Done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def user_course_status(status)
+    case status
+    when "pending"
+      "¡Empezá este curso!"
+    when "questions_completed"
+      "Solo te falta el proyecto final, ¿qué esperás?"
+    when "project_completed"
+      "Esperando feedback del equipo."
+    when "finished"
+      "Descargar certificado."
+    else
+      ""
+    end
+  end
 end
